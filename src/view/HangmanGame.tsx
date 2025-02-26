bba785331e682114319d3c901d5c777a3d1bddd7
AntonioPMCS <apmcarmosilva@ist.utl.pt>
2025-02-26 14:17:55 -0300

    solved compile bugs

--- src/view/HangmanGame.tsx
+++ src/view/HangmanGame.tsx
@@ -1,5 +1,3 @@
-import { useCallback, useEffect, useState } from "react";
-
 function HangManGame() {
   return (
     <div className="bg-white text-2xl flex flex-col items-center justify-center w-full mx-0 ">
