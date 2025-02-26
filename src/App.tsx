ec20bc7770b6676459fd6dcd5765f522f350f043
AntonioPMCS <apmcarmosilva@ist.utl.pt>
2025-02-26 12:01:10 -0300

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,28 @@
-import { useState, useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import './App.css'
-import UpProvider from './context/UpProvider';
 import UpProviderContext from './context/UpProviderContext';
-
-// Import the LUKSO web-components library
-let promise: Promise<unknown> | null = null;
-if (typeof window !== "undefined") {
-  promise = import("@lukso/web-components");
-}
 
 function App() {
   // Track connected accounts
   const {
     accounts, 
     contextAccounts, 
-    walletConnected
+    walletConnected,
+    chainId
   } = useContext(UpProviderContext)
 
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    // Load web component here if needed
-    promise?.then(() => {
-      setMounted(true);
-    });
-  }, []);
-
-  if (!mounted) {
-    return null; // or a loading placeholder
-  }
-
   return (
-    <UpProvider>
+    <main>
       <h2>
         Grid Host: {contextAccounts[0]}
       </h2>
       <h2>
         Connected Profile: {walletConnected? 'True' : 'False'} - {accounts[0]}
       </h2>
-    </UpProvider>
+      <h2>
+        chainId: {chainId}
+      </h2> 
+    </main>
   )
 }
 
